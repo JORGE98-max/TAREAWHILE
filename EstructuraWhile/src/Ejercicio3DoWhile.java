19002cff94f889f92f04c855820c97713688c2ed
Your Name <you@example.com>
2020-05-27 15:15:57 -0500

    Updated

--- EstructuraWhile/src/Ejercicio3DoWhile.java
+++ EstructuraWhile/src/Ejercicio3DoWhile.java
@@ -4,24 +4,26 @@ public class Ejercicio3DoWhile {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
+		int edad, i, prom, cantidad, sum;
 		
-		int edad, i, prom, cantidad;
-		
+		sum = 0;
+
 		prom = 0;
-		
+
 		i = 1;
 		System.out.println("Ingrese la cantidad de edades: ");
 		cantidad = scan.nextInt();
 		do {
-			
-			System.out.println("Ingrese la edad "+ i);
+
+			System.out.println("Ingrese la edad " + i);
 			edad = scan.nextInt();
-			i+=1;
-			prom = (prom + edad)/i;
-		}
-		while ( i <= cantidad);
-		
-		System.out.println("El promedio de las edades ingresadas es : "+ prom);
+			i += 1;
+			sum = sum + edad;
+			prom = sum / i;
+		} while (i <= cantidad);
+
+		System.out.println("El promedio de las edades ingresadas es : " + prom);
 
 	}
 
--- EstructuraWhile/src/EjercicioDoWhile.java
+++ EstructuraWhile/src/EjercicioDoWhile.java
@@ -4,23 +4,19 @@ public class EjercicioDoWhile {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int num, sumnum;
-		
+
 		sumnum = 0;
-	
-		
-		
+
 		do {
 			System.out.println("Ingrese el numero:");
-		    num = scan.nextInt();
-		    sumnum = sumnum + num;
-	    
-		}		
-		while (num != 0);
-		
-		System.out.println("la suma de los numeros ingresados es : "+sumnum);
-		
+			num = scan.nextInt();
+			sumnum = sumnum + num;
+
+		} while (num != 0);
+
+		System.out.println("la suma de los numeros ingresados es : " + sumnum);
 
 	}
 
