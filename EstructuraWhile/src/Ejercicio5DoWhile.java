65c440fbc5b7034838b420b8dc66e4cc02b45166
Your Name <you@example.com>
2020-05-28 14:40:36 -0500

    Updated

---
+++ EstructuraWhile/src/Ejercicio5DoWhile.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio5DoWhile {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int x, y, z, cantidad, i;
+		
+		x = 0;
+		y = 1;
+		z = 1;
+		i = 0;
+		
+		
+		System.out.println("Ingrese la cantidad de elementos : ");
+		cantidad = scan.nextInt();
+		
+		do {
+			i++;
+			z = x + y;
+		System.out.println(z);
+		x = y;
+		y = z;
+		
+		}
+		while(i < cantidad);
+		
+			
+	
+	}
+
+}
